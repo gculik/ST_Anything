528038bb80ad977ad58e7a6932bbc734f88134b9
Jiri Culik <gculik@imprimislc.com>
2017-08-29 20:04:30 -0400

    Comment Update

--- Arduino/libraries/ST_Anything/S_MomentarySwitch.cpp
+++ Arduino/libraries/ST_Anything/S_MomentarySwitch.cpp
@@ -11,7 +11,7 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  *  Authors: Jiri Culik
- *  Summary:  EX_MomentarySwitch is a class which implements the SmartThings "Momentary" and "Switch" device capability.
+ *  Summary:  S_MomentarySwitch is a class which implements the SmartThings "Momentary" and "Switch" device capability.
  */
 #include "S_MomentarySwitch.h"
 
